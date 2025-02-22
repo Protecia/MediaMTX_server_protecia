2a6752ef4874ac602c4d2a6214448e132d27eebd
Julien JEHL <julien.jehl@roboticia.com>
2025-02-22 14:46:08 +0100

    add cmd gstreamer with tls

--- gstreamer_cmd.sh
+++ gstreamer_cmd.sh
@@ -1,1 +1,4 @@
  gst-launch-1.0 rtspclientsink protocols=tcp name=s location=rtsp://<host>:8554/protecia filesrc location=video.mp4 ! qtdemux name=d d.video_0 ! queue ! s.sink_0 d.audio_0 ! queue ! s.sink_1
+
+# using tls:
+gst-launch-1.0 rtspclientsink protocols=tcp name=s location=rtsps://<host>:8555/protecia filesrc location=video.mp4 ! qtdemux name=d d.video_0 ! queue ! s.sink_0 d.audio_0 ! queue ! s.sink_1
