5b09eb50ad08835d11dc38443297605b922a3b29
Julien JEHL <julien.jehl@roboticia.com>
2025-05-03 11:08:43 +0200

    fix compose file

--- deploy/start_remote_media.sh
+++ deploy/start_remote_media.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # To start the docker compose
-docker compose -p mediamtx --env-file ../combined-$1.env -f docker-compose-$1.yaml up -d
+docker compose -p mediamtx --env-file ../combined-$1.env -f docker-compose-media-$1.yaml up -d
